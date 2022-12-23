06a81d5239cd7ef8e813a1cba44c806abd841cc8
loyalGunman <raykillerghostth@gmail.com>
2022-12-23 15:48:32 +0700

    lab9_3.cpp Completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,45 +11,34 @@ cout << "Enter your age: ";
 cin >> age;
     if (age <= 25)
     {
-        cout << "Enter your height: ";
-        cin >> high;
-            if (high < 100.00)
-            {
-                cout << "Your character = "<< "Chopper";
-            }
-            else if(high < 180.00)
-            {
-                cout << "Your character = "<< "Usopp";
-            }
-            else 
-            {
-                cout << "Enter your bounty: ";
-                cin >> bounty;
-                if (bounty > 1100000000.00) 
-                {
-                    cout << "Your character = "<< "Zoro";
-                }
-                else 
-                {
-                    cout << "Your character = "<<  "Sanji";
-                }
-            }
-    }
-    else if (age <= 60)
-    {
+    cout << "Enter your height: ";
+    cin >> high;
+        if (high < 100.00)
+        {
+        cout << "Your character = "<< "Chopper";
+        }else if(high < 180.00){
+        cout << "Your character = "<< "Usopp";
+        }else{
         cout << "Enter your bounty: ";
         cin >> bounty;
-        if (bounty > 500000000.00)
+        if (bounty > 1100000000.00) 
         {
-            cout << "Your character = "<< "Jinbe";
+        cout << "Your character = "<< "Zoro";
+        }else{
+        cout << "Your character = "<<  "Sanji";
         }
-        else
-        {
-            cout << "Your character = "<< "Franky";
         }
+    }else if (age <= 60){
+    cout << "Enter your bounty: ";
+    cin >> bounty;
+    if (bounty > 500000000.00)
+    {
+    cout << "Your character = "<< "Jinbe";
+    }else{
+    cout << "Your character = "<< "Franky";
     }
-    else {
-        cout << "Your character = "<< "Brook";
+    }else{
+    cout << "Your character = "<< "Brook";
     }
     return 0;
 }
