2c7ec38135314812004a817beda89e59fa814623
loyalGunman <raykillerghostth@gmail.com>
2022-12-23 15:50:11 +0700

    lab9_2.cpp Completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -8,17 +8,13 @@ char findGrade(double x)
     if( x >90.0)
     {
        return 'A' ;
-    }else if(x > 75.0)
-    {
+    }else if(x > 75.0){
         return 'B' ;
-    }else if(x > 60.0)
-    {
+    }else if(x > 60.0){
         return 'C' ;
-    }else if(x > 45.0)
-    {
+    }else if(x > 45.0){
         return 'D' ;
-    }else
-    {
+    }else{
         return 'F' ;
     }
 }
